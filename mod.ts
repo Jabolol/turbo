24f8ed7c175e4ccf200348b8e393241f56260224
Javier R <javier.rios-urbano@epitech.eu>
2023-07-04 10:51:37 +0200

    [add](mod.ts) Export `types` and `functions`

--- mod.ts
+++ mod.ts
@@ -1,5 +1,2 @@
-import { blue } from "https://deno.land/std@0.192.0/fmt/colors.ts";
-
-export default function turbo() {
-  console.log(`${blue("::")} turbo mode enabled`);
-}
+export { _symbols, find, free, init, load } from "./main.ts";
+export { type LibTurbo, type Routes } from "./types.ts";
