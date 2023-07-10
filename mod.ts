cdcb831b2f4ec50bb1998daaad1c7f5e465952bd
Javier R <javier.rios-urbano@epitech.eu>
2023-07-10 16:37:27 +0200

    [fix](mod.ts) `plug` not found

--- mod.ts
+++ mod.ts
@@ -1,3 +1,3 @@
 export { _symbols, find, free, init, load } from "./main.ts";
 export { type LibTurbo, type Routes } from "./types.ts";
-export { dlopen } from "plug";
+export { dlopen } from "https://deno.land/x/plug@1.0.2/mod.ts";
